401b9344a57e74b5916732cc5b4dee8cf18efd6b
phuoclv264 <phuoclv264@gmail.com>
2023-07-21 14:24:13 +0700

    Revert "Update PowerManagerService.java"
    This reverts commit 99a29f4c8d39d558e8e2d88436cbe28124881f98.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1402,9 +1402,6 @@ public final class PowerManagerService extends SystemService
 
     private void updateSettingsLocked() {
         final ContentResolver resolver = mContext.getContentResolver();
-            
-        // STAY_ON_WHILE_PLUGGED_IN always true
-        Settings.Global.putInt(resolver, Settings.Global.STAY_ON_WHILE_PLUGGED_IN, 1);
 
         mDreamsEnabledSetting = (Settings.Secure.getIntForUser(resolver,
                 Settings.Secure.SCREENSAVER_ENABLED,
@@ -3869,9 +3866,8 @@ public final class PowerManagerService extends SystemService
     }
 
     void setStayOnSettingInternal(int val) {
-        // STAY_ON_WHILE_PLUGGED_IN always true
         Settings.Global.putInt(mContext.getContentResolver(),
-                Settings.Global.STAY_ON_WHILE_PLUGGED_IN, 1);
+                Settings.Global.STAY_ON_WHILE_PLUGGED_IN, val);
     }
 
     void setMaximumScreenOffTimeoutFromDeviceAdminInternal(@UserIdInt int userId, long timeMs) {
