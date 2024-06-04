0d177899acf764507bc49f2087ec9a1e97377ab5
phuoclv264 <phuoclv264@gmail.com>
2024-06-04 15:31:26 +0700

    Update KrisLeeRef.java
    Test

--- core/java/android/os/KrisLeeRef.java
+++ core/java/android/os/KrisLeeRef.java
@@ -1,31 +1,16 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-@SuppressLint("MissingNullability")
 public class KrisLeeRef {
     private String deviceName;
-
     private static KrisLeeRef _instance;
 
-    @SuppressLint("MissingNullability")
+    @NonNull
     public static KrisLeeRef getInstance()
     {
         if (_instance == null)
         {
-            _instance = new MySingleton();
+            _instance = new KrisLeeRef();
         }
         return _instance;
     }
@@ -33,12 +18,11 @@ public class KrisLeeRef {
     private KrisLeeRef() {
     }
 
-    @SuppressLint("MissingNullability")
-    public void setDeviceName(String deviceName) {
+    public void setDeviceName(@NonNull String deviceName) {
         this.deviceName = deviceName;
     }
 
-    @SuppressLint("MissingNullability")
+    @Nullable
     public String getDeviceName() {
         return deviceName;
     }
