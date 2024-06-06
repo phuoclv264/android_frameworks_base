fb5f29cac1ae723f2ef7c8091899cef2150f476e
phuoclv264 <phuoclv264@gmail.com>
2024-06-06 18:13:36 +0700

    Update KrisLeeRef.java
    Test

--- core/java/android/provider/KrisLeeRef.java
+++ core/java/android/provider/KrisLeeRef.java
@@ -2,26 +2,35 @@ package android.provider;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.util.Slog;
 
 public class KrisLeeRef {
     private String deviceName;
-    private static KrisLeeRef _instance = new KrisLeeRef();
+    private static final String TAG = "KrisLeeRef";
+    private static final KrisLeeRef _instance = new KrisLeeRef();
 
     @NonNull
-    public static KrisLeeRef getInstance()
-    {
+    public static synchronized KrisLeeRef getInstance() {
+        Slog.w(TAG, "Getting instance: " + _instance);
+
+        if (_instance == null) {
+            _instance = new KrisLeeRef();
+        }
         return _instance;
     }
 
     private KrisLeeRef() {
+        Slog.w(TAG, "KrisLeeRef instance created");
     }
 
     public void setDeviceName(@NonNull String deviceName) {
+        Slog.w(TAG, "Setting device name: " + deviceName);
         this.deviceName = deviceName;
     }
 
     @Nullable
     public String getDeviceName() {
+        Slog.w(TAG, "Getting device name: " + deviceName);
         return deviceName;
     }
 }
