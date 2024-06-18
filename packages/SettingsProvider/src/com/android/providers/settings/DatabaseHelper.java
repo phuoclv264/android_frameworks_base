eb94cb3f447bbb6799038d285cd885181c239918
phuoclv264 <65098315+phuoclv264@users.noreply.github.com>
2024-06-18 15:49:29 +0700

    Update DatabaseHelper.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2473,7 +2473,9 @@ class DatabaseHelper extends SQLiteOpenHelper {
             loadSetting(stmt, Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
                     ("1".equals(SystemProperties.get("ro.kernel.qemu")) ||
                         res.getBoolean(R.bool.def_stay_on_while_plugged_in))
-                     ? 1 : 0);
+                     ? 7 : 0);
+
+            loadSetting(stmt, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 1)
 
             loadIntegerSetting(stmt, Settings.Global.WIFI_SLEEP_POLICY,
                     R.integer.def_wifi_sleep_policy);
