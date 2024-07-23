5f514b152462aa92f43b04ec1ca1c42e15461217
phuoclv264 <phuoclv264@gmail.com>
2024-07-23 22:02:14 +0700

    Revert "Update EthernetManager.java"
    This reverts commit 816411a1627a1288c87890e3b495910f4ea66406.

--- core/java/android/net/EthernetManager.java
+++ core/java/android/net/EthernetManager.java
@@ -189,7 +189,6 @@ public class EthernetManager {
     /**
      * Returns an array of available Ethernet interface names.
      */
-    @SuppressLint({"MissingNullability"})
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.R, trackingBug = 170729553)
     public String[] getAvailableInterfaces() {
         try {
