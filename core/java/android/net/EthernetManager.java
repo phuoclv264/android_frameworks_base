816411a1627a1288c87890e3b495910f4ea66406
phuoclv264 <phuoclv264@gmail.com>
2024-07-23 17:01:15 +0700

    Update EthernetManager.java

--- core/java/android/net/EthernetManager.java
+++ core/java/android/net/EthernetManager.java
@@ -189,6 +189,7 @@ public class EthernetManager {
     /**
      * Returns an array of available Ethernet interface names.
      */
+    @SuppressLint({"MissingNullability"})
     @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.R, trackingBug = 170729553)
     public String[] getAvailableInterfaces() {
         try {
